20d95b6d6d9945f37923e2b246b1e9f5c503b4a8
Ali Eren <devalierentabak@outlook.com>
2025-05-03 22:00:55 +0100

    editing templated models finished implemented some views but not tested manually...

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
@@ -34,3 +36,5 @@ class Comment(models.Model):
 
     def __str__(self):
         return 'Comment by {} on {}'.format(self.name, self.post)
+
+    
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect, get_list_or_404
+from .forms import CommentForm
+from .models import Post, Comment
+from django.core.exceptions import ValidationError
 # Create your views here.
 def home(request):
-    return render(request, 'blog/index.html')
-
-def blog(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.filter(active = True)
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def about(request):
@@ -14,3 +14,24 @@ def about(request):
 
 def contact(request):
     return render(request, 'blog/contact.html')
+
+
+def all_posts(request):
+     return None
+
+def blog(request, post_id):
+        related_post = get_object_or_404(Post, post_id) # When using utility first model then parameter id taken
+        if request.method == "POST": 
+            form = CommentForm(request.POST)
+            if form.is_valid(): 
+                name  = form.cleaned_data["name"]
+                body = form.cleaned_data["body"]
+                Comment.objects.create(post= related_post, name = name, body = body)
+                return redirect() #path of html and post id to redirect in that post
+        else:
+            return CommentForm() 
+
+        
+
+
+
